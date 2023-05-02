43f44f292c30531a1e3f5bd62083a6b89514ce39
shailesh-katonic <103512924+shailesh-katonic@users.noreply.github.com>
2023-05-02 14:18:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+# apt-get install -y locales
+# echo "en_US UTF-8" > /etc/locale.gen
+# locale-gen en_US.UTF-8
+# export LANG=en_US.UTF-8
+# export LANGUAGE=en_US:en
+# export LC_ALL=en_US.UTF-8
