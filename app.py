5accf6d866a6d7e71e14078dc4fdfdcccb108c47
shailesh-katonic <103512924+shailesh-katonic@users.noreply.github.com>
2023-12-01 18:59:20 +0530

    test: Testing write permission

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
+os.makedirs("test")
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
